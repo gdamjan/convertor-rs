f576b583c2f8bd47402dee34a7f1725790cfebe3
Дамјан Георгиевски <gdamjan@gmail.com>
2022-01-30 23:16:00 +0100

    extract plain text from office:body

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use quick_xml::Reader;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
-fn list_styles(styles: impl Read) -> std::io::Result<()> {
+fn list_styles_with_fonts(styles: impl Read) -> std::io::Result<()> {
     let reader = BufReader::new(styles);
     let mut reader = Reader::from_reader(reader);
 
@@ -11,24 +11,68 @@ fn list_styles(styles: impl Read) -> std::io::Result<()> {
     loop {
         match reader.read_event(&mut buf) {
             Ok(Event::Empty(ref e)) => match e.name() {
-              b"style:text-properties" => {
-                let v = e.attributes()
-                    .filter_map(|a| a.ok().filter(|a| a.key == b"style:font-name").map(|a| a.value))
-                    .collect::<Vec<_>>();
-                if v.len() > 0 {
-                  let font_name = String::from_utf8_lossy(&v[0]);
-                  println!("{}: {}", String::from_utf8_lossy(e.name()), font_name);
+                b"style:text-properties" => {
+                    let v = e
+                        .attributes()
+                        .filter_map(|a| a.ok().filter(|a| a.key == b"style:font-name").map(|a| a.value))
+                        .collect::<Vec<_>>();
+                    if v.len() > 0 {
+                        let font_name = String::from_utf8_lossy(&v[0]);
+                        println!("{}: {}", String::from_utf8_lossy(e.name()), font_name);
+                    }
                 }
-              },
-              _ => ()
+                _ => (),
             },
-            // Ok(Event::Text(e)) => txt.push(e.unescape_and_decode(&reader).unwrap()),
             Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
             Ok(Event::Eof) => break,
             _ => (),
         }
         buf.clear();
     }
+
+    Ok(())
+}
+
+fn inside_body<B>(reader: &mut Reader<B>)
+where
+    B: BufRead,
+{
+    let mut buf = Vec::new();
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Text(ref e)) => {
+                println!("{}", e.unescape_and_decode(&reader).unwrap())
+            }
+            Ok(Event::End(ref e)) => match e.name() {
+                b"office:body" => break,
+                _ => (),
+            },
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Ok(Event::Eof) => break,
+            _ => (),
+        }
+        buf.clear();
+    }
+}
+
+fn print_content_body(content: impl Read) -> std::io::Result<()> {
+    let reader = BufReader::new(content);
+    let mut reader = Reader::from_reader(reader);
+
+    let mut buf = Vec::new();
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) => match e.name() {
+                b"office:body" => inside_body(&mut reader),
+                _ => (),
+            },
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Ok(Event::Eof) => break,
+            _ => (),
+        }
+        buf.clear();
+    }
+
     Ok(())
 }
 
@@ -37,9 +81,10 @@ fn main() -> std::io::Result<()> {
     let mut zip = zip::ZipArchive::new(file)?;
 
     let styles = zip.by_name("styles.xml")?;
-    list_styles(styles)?;
+    list_styles_with_fonts(styles)?;
 
-    //let contents = zip.by_name("contents.xml")?;
+    let content = zip.by_name("content.xml")?;
+    print_content_body(content)?;
 
     Ok(())
 }
