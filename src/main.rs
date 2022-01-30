edba24f55bbc957b0bdd1f17c81ad00efaafbea2
Дамјан Георгиевски <gdamjan@gmail.com>
2022-01-30 22:50:46 +0100

    list of fonts in style:text-properties

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,20 @@ fn list_styles(styles: impl Read) -> std::io::Result<()> {
     let reader = BufReader::new(styles);
     let mut reader = Reader::from_reader(reader);
 
-    // let mut count = 0;
-    // let mut txt = Vec::new();
     let mut buf = Vec::new();
     loop {
         match reader.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) => {
-              println!("{:?}", String::from_utf8_lossy(e.name()));
+            Ok(Event::Empty(ref e)) => match e.name() {
+              b"style:text-properties" => {
+                let v = e.attributes()
+                    .filter_map(|a| a.ok().filter(|a| a.key == b"style:font-name").map(|a| a.value))
+                    .collect::<Vec<_>>();
+                if v.len() > 0 {
+                  let font_name = String::from_utf8_lossy(&v[0]);
+                  println!("{}: {}", String::from_utf8_lossy(e.name()), font_name);
+                }
+              },
+              _ => ()
             },
             // Ok(Event::Text(e)) => txt.push(e.unescape_and_decode(&reader).unwrap()),
             Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
@@ -26,13 +33,13 @@ fn list_styles(styles: impl Read) -> std::io::Result<()> {
 }
 
 fn main() -> std::io::Result<()> {
-    let file = File::open("problem.odt")?;
+    let file = File::open("fixtures/problem.odt")?;
     let mut zip = zip::ZipArchive::new(file)?;
 
     let styles = zip.by_name("styles.xml")?;
-    list_styles(styles);
+    list_styles(styles)?;
 
-    let contents = zip.by_name("contents.xml")?;
+    //let contents = zip.by_name("contents.xml")?;
 
     Ok(())
 }
