78be43970f74bf80b9c6828a27f35a8bf0007126
Дамјан Георгиевски <gdamjan@gmail.com>
2022-01-31 04:06:16 +0100

    match guards, and copy styles.xml to output

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,29 @@ use std::io::{BufReader, BufRead, Read, Write};
 
 /// Find all styles that use the YUSCII fonts, and convert them to use normal fonts
 /// Keep track what styles were converted and which font was used
-fn convert_styles(styles: impl Read, _converted: &mut impl Write) -> std::io::Result<()> {
+fn convert_styles(styles: impl Read, converted: &mut impl Write) -> std::io::Result<()> {
   let reader = BufReader::new(styles);
   let mut reader = Reader::from_reader(reader);
 
+  let mut writer = Writer::new(converted);
   let mut buf = Vec::new();
   loop {
     buf.clear();
     match reader.read_event(&mut buf) {
-      Ok(Event::Empty(ref e)) => match e.name() {
-        b"style:text-properties" => {
-          let font_name = e.attributes()
-            .filter_map(|res| res.ok())
-            .find(|a| a.key == b"style:font-name");
-
-          if let Some(font_name) = font_name {
-            let font_name = String::from_utf8_lossy(&font_name.value);
-            let e_name = String::from_utf8_lossy(e.name());
-            println!("{}: {}", e_name, font_name);
-          }
-        }
-        _ => (),
-      },
       Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
       Ok(Event::Eof) => break,
-      _ => (),
+      Ok(Event::Empty(ref e)) if e.name() == b"style:text-properties" => {
+        let font_name = e.attributes()
+          .filter_map(|res| res.ok())
+          .find(|a| a.key == b"style:font-name");
+
+        if let Some(font_name) = font_name {
+          let font_name = String::from_utf8_lossy(&font_name.value);
+          let e_name = String::from_utf8_lossy(e.name());
+          println!("{}: {}", e_name, font_name);
+        }
+      },
+      Ok(e) => assert!(writer.write_event(&e).is_ok()),
     }
   }
 
@@ -49,7 +47,8 @@ where
       Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
       Ok(Event::Eof) => break, // panic ? we really should see End of office:body
       Ok(Event::Text(ref e)) => {
-        println!("{}", e.unescape_and_decode(&reader).unwrap())
+        println!("{}", e.unescape_and_decode(&reader).unwrap());
+        assert!(writer.write_event(Event::Text(e.clone())).is_ok());
       },
       Ok(Event::End(ref e)) => {
         assert!(writer.write_event(Event::End(e.clone())).is_ok());
@@ -63,7 +62,7 @@ where
 fn convert_content(content: impl Read, converted: &mut impl Write) -> std::io::Result<()> {
   let reader = BufReader::new(content);
   let mut reader = Reader::from_reader(reader);
-  reader.trim_text(true); // FIXME: remove from the final version
+  // reader.trim_text(true); // FIXME: remove from the final version
 
   let mut writer = Writer::new(converted);
   let mut buf = Vec::new();
@@ -72,11 +71,9 @@ fn convert_content(content: impl Read, converted: &mut impl Write) -> std::io::R
     match reader.read_event(&mut buf) {
       Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
       Ok(Event::Eof) => break,
-      Ok(Event::Start(ref e)) => {
+      Ok(Event::Start(ref e)) if e.name() == b"office:body" => {
         assert!(writer.write_event(Event::Start(e.clone())).is_ok());
-        if e.name() == b"office:body" {
-          convert_body(&mut reader, &mut writer);
-        };
+        convert_body(&mut reader, &mut writer);
       },
       Ok(e) => assert!(writer.write_event(&e).is_ok()),
     }
